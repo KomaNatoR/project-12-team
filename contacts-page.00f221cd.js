e6bdfe3bea4af67810b5cada025dfaded9279da6
KomaNatoR <108286639+KomaNatoR@users.noreply.github.com>
2022-09-15 05:35:25 +0000

    Deploying to gh-pages from @ KomaNatoR/project-12-team@871906e8f2b30b5d94121faafcce0d9940ccdba6 🚀

---
+++ contacts-page.00f221cd.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector(".menu-open"),closeMenuBtn:document.querySelector(".menu-close"),menu:document.querySelector(".nav-menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=contacts-page.00f221cd.js.map
---
+++ contacts-page.9ed9d154.js
@@ -0,0 +1,2 @@
+!function(){var t={openContactsBtn:document.querySelector(".contacts-button"),closeContactsBtn:document.querySelector(".contacts-button"),contacts:document.querySelector(".contacts__menu")};function n(){t.contacts.classList.toggle("is-hidden")}t.openContactsBtn.addEventListener("click",n),t.closeContactsBtn.addEventListener("click",n)}();
+//# sourceMappingURL=contacts-page.9ed9d154.js.map
